1aa3cb6857a8c08eefcb585a8a9a14de79569e95
Marcos <marcosmolero@mac-mini-de-marcos.local>
2017-02-22 09:52:33 +0100

    1.0.0 (1) - Creación InitVC.

---
+++ iBooChallenge/InitVC.swift
@@ -0,0 +1,30 @@
+//
+//  InitVC.swift
+//  iBooChallenge
+//
+//  Created by Marcos on 22/2/17.
+//  Copyright © 2017 marcosmolero. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InitVC: UIViewController {
+    
+    
+    // ===================================================================================
+    // MARK:                    OVERRIDE FUNC
+    // ===================================================================================
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
